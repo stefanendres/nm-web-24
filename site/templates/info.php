0465c7c59c09ff499a2170d43b2193fdeed6e8de
Stefan Endres <info@stefanendres.com>
2024-01-18 16:08:41 +0100

    setup template test

--- site/templates/info.php
+++ site/templates/info.php
@@ -1,8 +1,26 @@
 <?php snippet('head'); ?>
-<?php //snippet('header'); ?>
 <main>
-  info template
-  <?php //snippet('footer'); ?>
+  <h2 class="headline">
+    <?php echo $page->about_headline() ?>
+  </h2>
+  <div class="textfield">
+    <?php echo $page->about_text() ?>
+  </div>
+
+
+  <ul>
+    <?php foreach($page->teaching_staff()->toStructure() as $list_element): ?>
+      <li>
+        <?php if ($list_element->profile_picture()->isNotEmpty()) : ?>
+          <img class="profile-picture" src="<?php echo $list_element->profile_picture()->toFile()->url() ?>"/>
+        <?php endif ?>
+        <div><?= $list_element->name() ?></div>
+        <div><?= $list_element->role() ?></div>
+      </li>
+    <?php endforeach ?>
+  </ul>
+
+
+
 </main>
-<?php //snippet('cookie'); ?>
 <?php snippet('foot'); ?>
